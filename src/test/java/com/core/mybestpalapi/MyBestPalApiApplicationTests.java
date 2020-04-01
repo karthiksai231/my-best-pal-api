ca1b3d02bc74c890afd37fab10eeee1e6009218c
karthik pamidimukkala <karthiksai231@gmail.com>
2020-04-01 18:21:14 -0400

    Initial Project Commit

---
+++ src/test/java/com/core/mybestpalapi/MyBestPalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.mybestpalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBestPalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
